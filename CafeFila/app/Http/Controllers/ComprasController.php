c63f79210b2bdd97acb274cfc1df277fea05db24
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-11-10 21:47:19 -0300

    inicio da listagem de compras

--- CafeFila/app/Http/Controllers/ComprasController.php
+++ CafeFila/app/Http/Controllers/ComprasController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Compras;
 use Illuminate\Http\Request;
 
 class ComprasController extends Controller
 {
-    
+    public function listar(){
+        try {
+            $compras = Compras::with('usuario')->get();
+
+            return response()->json($compras, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao listar as compras dos usuários.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
--- CafeFila/app/Models/Compras.php
+++ CafeFila/app/Models/Compras.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Compras extends Model
 {
-    //
+    protected $table = "compra";
+
+    protected $primaryKey = "id";
+
+    public $timestamps = false;
 }
